ba3eef33fcac8e95c6c37b03c6703d93e4a8d404
btror <ticer1999@gmail.com>
2021-11-09 00:22:36 -0500

    created directory for old code

--- old/main_old.py
+++ old/main_old.py
@@ -1,13 +1,10 @@
-import nltk
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
-from nltk.sentiment import SentimentIntensityAnalyzer
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import classification_report
 from sklearn.svm import LinearSVC
 
 
@@ -27,9 +24,9 @@ def get_soup(url):
                 Chrome/90.0.4430.212 Safari/537.36',
                 'Accept-Language': 'en-US, en;q=0.5'})
 
-    # gather html from first 20 pages
+    # gather html from first 100 pages
     html_data = ""
-    for i in range(50):
+    for i in range(200):
         # adjust the link to loop through each page
         current_page = str("=" + str(int((i + 1))))
         next_page = str("=" + str(int((i + 2))))
@@ -60,20 +57,29 @@ def organize_data(soup):
         title = ""
         rating = ""
         description = ""
+        sentiment = ""
         try:
             title = review.find("a", {"data-hook": "review-title"}).text.strip()
             rating = float(review.find("i", {"data-hook": "review-star-rating"}).text.replace("out of 5 stars", "")
                            .strip())
             description = review.find("span", {"data-hook": "review-body"}).text.replace("Read more", "").strip()
+            if rating < 3:
+                sentiment = "negative"
+            elif rating < 4:
+                sentiment = "neutral"
+            else:
+                sentiment = "positive"
         except AttributeError:
             pass
 
         # do not include reviews with empty data
-        if title != "" and rating != "" and description != "" and "The media could not be loaded." not in description:
+        if title != "" and rating != "" and description != "" and "The media could not be loaded." not in description \
+                and "a " not in description:
             review_dict = {
                 "title": title,
                 "rating": rating,
                 "description": description,
+                "sentiment": sentiment
             }
             reviews.append(review_dict)
 
@@ -81,66 +87,49 @@ def organize_data(soup):
 
 
 def sentiment_analysis(dataframe):
-    # TODO: create a sentiment analysis ML algorithm. Determine if a positive or negative emotion is portrayed (Naive
-    #  Bayes) check this tutorial out: https://www.twilio.com/blog/2017/12/sentiment-analysis-scikit-learn.html
-    data = dataframe["description"]
-    data_ratings = dataframe["rating"]
+    """
+    Train and test data.
+
+    :param dataframe: A dataframe object containing reviews.
+    :type dataframe: DataFrame
+    """
 
     tfidf = TfidfVectorizer(max_features=20000, ngram_range=(1, 5), analyzer="char")
     X = tfidf.fit_transform(dataframe["description"])
-    y = df["rating"]
+    y = df["sentiment"]  # can change to "rating" to guess 1-5 star instead of negative/positive
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    clf = LinearSVC(C=20, class_weight="balanced")
+    clf = LinearSVC(C=20, class_weight="balanced", max_iter=20000)
     clf.fit(X_train, y_train)
 
     y_pred = clf.predict(X_test)
     print(classification_report(y_test, y_pred))
 
+    y_test = y_test.tolist()
+    y_pred = y_pred.tolist()
+
+    custom = "This is a really good game. No problems at all."
+    custom = tfidf.transform([custom])
+    print("prediction - ", clf.predict(custom))
+
+    plt.plot(y_test, "3", color="red", label="sample rating")
+    plt.plot(y_pred, "4", color="blue", label="predicted rating")
+
+    for i in range(len(y_test)):
+        plt.vlines(x=i, ymin=y_test[i], ymax=y_pred[i], color="black")
+
+    plt.legend(numpoints=1)
+    plt.show()
 
 
+def save_data(link):
+    """
+    Save the updated data to the csv file.
 
-    # vectorizer = CountVectorizer(
-    #     analyzer="word",
-    #     lowercase=False,
-    # )
-    # features = vectorizer.fit_transform(
-    #     data
-    # )
-    # features_nd = features.toarray()
-    #
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     features_nd,
-    #     data_ratings,
-    #     train_size=.1,
-    #     random_state=0)
-    #
-    # log_model = LogisticRegression()
-    # log_model = log_model.fit(X=X_train, y=y_train)
-    # y_pred = log_model.predict(X_test)
-    #
-    # print(accuracy_score(y_test, y_pred))
-    # print(y_pred)
-    #
-    # plt.figure(figsize=(8, 5))
-    # plt.title("Multiple Features", loc="left")
-    # plt.xlabel("Predicted")
-    # plt.ylabel("Actual")
-    #
-    # plt.plot([0, 20], [0, 20], color="red")
-    #
-    # plt.scatter(y_pred, y_test, color="blue", marker="+")
-    #
-    # plt.xlim([0, 20])
-    # plt.ylim([0, 20])
-    #
-    # plt.show()
-
-
-if __name__ == '__main__':
-    # link for ZOTAC GAMING GeForce GTX 1650 graphics card
-    link = "https://www.amazon.com/Xbox-Wireless-Controller-Pulse-Red-Windows-Devices/product-reviews/B0859XT328/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1"
+    :param link: The link to the product review.
+    :type link: str
+    """
 
     # scrape reviews from Amazon.com
     html = get_soup(link)
@@ -149,18 +138,20 @@ if __name__ == '__main__':
     customer_reviews = organize_data(html)
 
     # create a dataframe and write review data to csv file
-    df = pd.DataFrame(customer_reviews)
-    df.to_csv("data/amazon_review_data.csv", index=False)
+    dataframe = pd.DataFrame(customer_reviews)
+    dataframe.to_csv("data/amazon_review_data.csv", index=False)
 
-    # gather 7 reviews from every rating
-    # size = df[df.rating == 2.0].shape[0]
-    # one_star = df[df.rating == 1.0].head(size)
-    # two_star = df[df.rating == 2.0].head(size)
-    # three_star = df[df.rating == 3.0].head(size)
-    # four_star = df[df.rating == 4.0].head(size)
-    # five_star = df[df.rating == 5.0].head(size)
 
-    # df = pd.concat([one_star, two_star, three_star, four_star, five_star])
+if __name__ == '__main__':
+    # link to Amazon product reviews
+    product_link = "https://www.amazon.com/Xbox-Wireless-Controller-Pulse-Red-Windows-Devices/product-reviews" \
+                   "/B0859XT328/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1"
+
+    # uncomment line below to scrape latest Amazon review data from the product link above
+    # save_data(product_link)
+
+    # read amazon review data from the csv file
+    df = pd.read_csv("data/amazon_review_data.csv")
 
     # perform a ML sentiment analysis
     sentiment_analysis(df)
